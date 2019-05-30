908ad7bd75ecdec248e5c0966692accc4316494c
ilianakyl <ilianakyl@gmail.com>
2019-05-30 20:26:37 +0300

    add html files and actions on save button

--- scripts/popup.js
+++ scripts/popup.js
@@ -17,23 +17,36 @@ console.log('here')
         // Don't follow the link
         event.preventDefault();
 
-        console.log(window.location)
+        let url = window.location.protocol+"//"+window.location.host
         let shortcode = window.location.pathname.replace(/\/j\//,'');
-        // let chrome_storage_key = `job_data_${shortcode}`
+        let chrome_storage_key = `job_data_${shortcode}`
         
-        // fetch(`http://batesmotel.lvh.me:3000/spi/v3/get/jobs/${shortcode}/`, {
-        //     headers: {"Authorization": "Bearer 92f8b94a1d6ef0d31c4bcc3dfe5cc9102a676b5ccfe8443f9ee4fe16c5fee598"},
-        //     type: 'GET',
-        //     Accept: "application/json",
-        //     contentType: "application/json",
-        // })
-        // .then(response => response.json())
-        // .then(response => {
-        //   chrome.storage.sync.set({key: response}, function() {
-        //     console.log(key);
-        //     console.log(response);
-        //   });
-        // });
+        fetch(`${shortcode}/spi/v3/get${shortcode}/`, {
+            headers: {"Authorization": "Bearer 49d962d93e03722c70359d8910e5d870b005f23312552df900b25ebed775672b"},
+            type: 'GET',
+            Accept: "application/json",
+            contentType: "application/json",
+        })
+        .then(response => response.json())
+        .then(response => {
+          chrome.storage.sync.set({key: response}, function() {
+            console.log(key);
+            console.log(response);
+          });
+        });
+        var new_div  = document.getElementById ('hestiaDiv');
+        var xhr = new XMLHttpRequest();
+  
+    xhr.onreadystatechange = function (e) { 
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        new_div.innerHTML = xhr.responseText;
+      }
+    }
+  
+    xhr.open("GET", chrome.extension.getURL("frames/list.html"), true);
+    xhr.setRequestHeader('Content-type', 'text/html');
+    xhr.send();
+
     }
 	
 
